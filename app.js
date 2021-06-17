8eabb38440ddf05d2a78afe1fde35aa67e22d967
ghulamghousdev <itsghulamghous@gmail.com>
2021-06-17 22:21:08 +0500

    integrated database and user auth

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
+require("./database/mongoose");
 const app = express();
 const cors = require("cors");
+const UserRouter = require("./routes/User");
 
 
 //To recognize incoming requests as json object.
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(UserRouter);
 
 //Defining Port
 const port = process.env.Port || 5000;
