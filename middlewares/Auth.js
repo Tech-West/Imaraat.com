9dcfc75f75053859c53f858f25ba640a7301c24c
ghulamghousdev <itsghulamghous@gmail.com>
2021-06-17 22:22:53 +0500

    added middlewares

---
+++ middlewares/Auth.js
@@ -0,0 +1,23 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+//AUTHENTICATION MIDDLEWARE
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, "thisisuserverification");
+    const user = await User.findOne({
+      _id: decoded._id,
+      "tokens.token": token,
+    });
+    if (!user) {
+      throw new Error("Please Authenticate");
+    }
+    req.user = user;
+    req.token = token;
+    next();
+  } catch (e) {
+    res.status(400).send({ error: "Please Authenticate" });
+  }
+};
+
+module.exports = auth;
