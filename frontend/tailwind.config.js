8a57efbd2c8c405230108bac3b874cbbfa9a8fe9
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2021-06-22 15:36:10 -0700

    Defined theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,27 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        "login-bg": "url('./images/login-bg.jpg')",
+        "register-bg": "url('./images/register-bg.jpg')",
+        "primary-gradient": "linear-gradient(45deg, #FF5F1F, #FF4433)",
+      }),
+
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+        "light-op-09": "rgba(255,255,255,0.8)",
+      }),
+      colors: {
+        orange: {
+          primary: "#FF5F1F",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
