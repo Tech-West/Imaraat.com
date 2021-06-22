32204bc410ff643816f163150f3445deccf251b5
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2021-06-22 15:41:28 -0700

    Routes Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import store from "./store";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="text-center">
-        Imaraat.com
-      </div>
+      <Router>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </Router>
     </Provider>
   );
 }
