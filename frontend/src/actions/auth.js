7d654aa3899520c4e65cd7a78fb4e2ef9fa29ce4
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2021-06-23 14:32:32 -0700

    Added: login action

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -1,32 +1,61 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "./actionTypes";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+} from "./actionTypes";
 import axios from "axios";
 import setGlobalAuthToken from "../utils/setToken";
 
-export const register = (username, email, password)=> async (dispatch) => {
-    const config = {
-        header: {
-          "Content-Type": "application/json",
-        },
-    };
+export const register = (username, email, password) => async (dispatch) => {
+  const config = {
+    header: {
+      "Content-Type": "application/json",
+    },
+  };
 
-    const body = {
-        username, 
-        email, 
-        password,
-    };
+  const body = {
+    username,
+    email,
+    password,
+  };
 
-    try{
-        const res = axios.post('/api/register', body, config);
-        setGlobalAuthToken(res.data.token);
-        console.log(res.data);
-        dispatch({
-            type: REGISTER_SUCCESS,
-            payload: res.data,
-        });
+  try {
+    const res = await axios.post("/api/register", body, config);
+    setGlobalAuthToken(res.data.token);
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: REGISTER_FAIL,
+    });
+  }
+};
 
-    } catch(err){
-        dispatch({
-            type: REGISTER_FAIL,
-        });
-    }
-} 
+export const login = (email, password) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const body = {
+    email,
+    password,
+  };
+
+  try {
+    const res = await axios.post("/api/login", body, config);
+    setGlobalAuthToken(res.data.token);
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGIN_FAIL,
+    });
+  }
+};
--- frontend/src/reducers/auth.js
+++ frontend/src/reducers/auth.js
@@ -1,31 +1,36 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "../actions/actionTypes";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+} from "../actions/actionTypes";
 
 const initialState = {
-    token: null,
-    isAuthenticated: null,
-    isLoading: true,
-    userInfo: null
-}
+  token: null,
+  isAuthenticated: null,
+  isLoading: true,
+  userInfo: null,
+};
 
-const auth = (auth = initialState, action) =>{
-    const {type, payload} = action;
-    if(type === REGISTER_SUCCESS){
-        return {
-            ...auth,
-            token: payload.token,
-            isAuthenticated: true,
-            isLoading: false,
-            userInfo: payload.user,
-        }
-    } else if(type === REGISTER_FAIL){
-        return {
-            token: null,
-            isAuthenticated: false,
-            isLoading: false,
-            userInfo: null,
-        }
-    }
-    return auth;
-}
+const auth = (auth = initialState, action) => {
+  const { type, payload } = action;
+  if (type === REGISTER_SUCCESS || type === LOGIN_SUCCESS) {
+    return {
+      ...auth,
+      token: payload.token,
+      isAuthenticated: true,
+      isLoading: false,
+      userInfo: payload.user,
+    };
+  } else if (type === REGISTER_FAIL || LOGIN_FAIL) {
+    return {
+      token: null,
+      isAuthenticated: false,
+      isLoading: false,
+      userInfo: null,
+    };
+  }
+  return auth;
+};
 
 export default auth;
