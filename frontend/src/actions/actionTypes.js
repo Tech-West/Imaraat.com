29efad39be8a2b24e61ecd0b8866b25ba3503c44
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2021-06-25 13:48:36 -0700

    Added: types for Alerts

--- frontend/src/actions/actionTypes.js
+++ frontend/src/actions/actionTypes.js
@@ -2,3 +2,5 @@ export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
 export const REGISTER_FAIL = "REGISTER_FAIL";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
+export const SET_ALERTS = "SET_ALERTS";
+export const REMOVE_ALERTS = "REMOVE_ALERTS";
