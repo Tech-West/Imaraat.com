a8961edc9be75513cf82d650c099bbe152e87d59
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2021-07-22 14:48:15 -0700

    Used Profile Router

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require("cors");
 const UserRouter = require("./routes/User");
 const UploadRouter = require("./routes/upload");
+const ProfileRouter = require("./routes/profile");
 
 //To recognize incoming requests as json object.
 app.use(cors());
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(UserRouter);
 app.use(UploadRouter);
+app.use(ProfileRouter);
 
 //Defining Port
 const port = process.env.Port || 5000;
