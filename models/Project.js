6dda324c48d9eb6cb9620a228fab766461809599
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2021-07-22 13:04:49 -0700

    Added Project Schema

---
+++ models/Project.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const projectSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+
+  description: {
+    type: String,
+  },
+
+  location: {
+    type: String,
+    required: true,
+  },
+
+  rooms: {
+    type: Number,
+    required: true,
+  },
+
+  status: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!(value === "Complete" || value === "Progress" || value === "Sold")) {
+        throw new Error("Status not valid");
+      }
+    },
+  },
+
+  area: {
+    type: Number,
+    required: true,
+  },
+
+  coverImageUrl: {
+    type: String,
+    required: true,
+  },
+
+  gallery: [
+    {
+      imageUrl: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+const project = mongoose.model("Project", projectSchema);
+
+module.exports = project;
