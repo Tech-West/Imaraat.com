b6e78fccc284c87db7ef0c1c8b0a6a0d1473b36c
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2021-07-22 13:08:52 -0700

    Route setup for Profile creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,24 @@ import store from "./store";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import CreateProfile from "./components/Profile/CreateProfile";
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/" exact component={Home} />
-        </Switch>
-      </Router>
-    </Provider>
+    <div className="transition duration-500">
+      <Provider store={store}>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/profile" component={CreateProfile} />
+            <Route path="/" exact component={Home} />
+          </Switch>
+        </Router>
+      </Provider>
+    </div>
   );
 }
 
