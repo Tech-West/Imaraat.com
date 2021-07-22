185eebcfb502b5e44ae767852e333c3c03a1bfcc
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2021-07-22 13:07:06 -0700

    Used upload router

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ require("./database/mongoose");
 const app = express();
 const cors = require("cors");
 const UserRouter = require("./routes/User");
-
+const UploadRouter = require("./routes/upload");
 
 //To recognize incoming requests as json object.
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(UserRouter);
+app.use(UploadRouter);
 
 //Defining Port
 const port = process.env.Port || 5000;
 const server = app.listen(port, () => {
   console.log(`Imaraat listening at http://localhost:${port}`);
-}); 
+});
