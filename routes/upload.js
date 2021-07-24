d85522e10d0101a1d935665eb66ee00b2e49e59b
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2021-07-24 12:44:49 -0700

    Improved route for getting a signed url

--- routes/upload.js
+++ routes/upload.js
@@ -7,24 +7,25 @@ const uuid = require("uuid").v1;
 const router = new express.Router();
 
 const s3 = new AWS.S3({
+  region: "eu-central-1",
   accessKeyId: keys.accessKeyId,
   secretAccessKey: keys.secretAccessKey,
+  signatureVersion: "v4",
 });
 
-router.get("/api/upload", auth, (req, res) => {
+router.get("/api/upload", auth, async (req, res) => {
   const key = `${req.user.id}/${uuid()}.jpeg`;
 
-  s3.getSignedUrl(
-    "putObject",
-    {
-      Bucket: "imaraat-bucket",
-      ContentType: "jpeg",
+  try {
+    const uploadUrl = await s3.getSignedUrlPromise("putObject", {
+      Bucket: "s3-imaraat-bucket",
       Key: key,
-    },
-    (err, url) => {
-      res.send({ key, url });
-    }
-  );
+      Expires: 60,
+    });
+    res.status(200).send({ url: uploadUrl });
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
 module.exports = router;
