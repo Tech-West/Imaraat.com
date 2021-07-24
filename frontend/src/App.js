b8db4890f621363ad64a895a3002774c6d2d7405
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2021-07-24 12:45:16 -0700

    Added route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Register from "./components/auth/Register";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import CreateProfile from "./components/Profile/CreateProfile";
+import UploadAvatar from "./components/Profile/UploadAvatar";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
             <Route path="/login" component={Login} />
             <Route path="/register" component={Register} />
             <Route path="/profile" component={CreateProfile} />
+            <Route path="/avatar/upload" component={UploadAvatar} />
             <Route path="/" exact component={Home} />
           </Switch>
         </Router>
