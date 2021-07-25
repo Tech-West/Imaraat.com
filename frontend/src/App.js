12b7442afc92fadcc6807e6f1d7be99f7ad56970
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2021-07-25 09:40:16 -0700

    Added header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import store from "./store";
@@ -6,12 +7,21 @@ import Register from "./components/auth/Register";
 import Home from "./components/Home/Home";
 import CreateProfile from "./components/Profile/CreateProfile";
 import UploadAvatar from "./components/Profile/UploadAvatar";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
+import { loadUser } from "./actions/auth";
 
 function App() {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <div className="transition duration-500">
       <Provider store={store}>
         <Router>
+          <Navbar />
           <Switch>
             <Route path="/login" component={Login} />
             <Route path="/register" component={Register} />
@@ -19,6 +29,7 @@ function App() {
             <Route path="/avatar/upload" component={UploadAvatar} />
             <Route path="/" exact component={Home} />
           </Switch>
+          <Footer />
         </Router>
       </Provider>
     </div>
