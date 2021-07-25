af28d44b4d737bf3f7b74e2b83fc1b58d18adaa1
Ahmad Zaheer <ahmadzaheerdev@gmail.com>
2021-07-25 09:39:42 -0700

    Refactored

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,26 +1,15 @@
 import { Fragment } from "react";
 import { connect } from "react-redux";
 import Navbar from "../Navbar/Navbar";
-import HeroBanner from "../Banner/HeroBanner"
+import HeroBanner from "../Banner/HeroBanner";
 import ListingBanner from "../Banner/ListingBanner";
-import Footer from "../Footer/Footer"
+import Footer from "../Footer/Footer";
 
 const Home = ({ isLoading, isAuthenticated }) => {
   return (
-    <div
-      style={{ minHeight: "86vh" }}
-      className=""
-    >
-      <Navbar />
-      {!isLoading && isAuthenticated ? (
-        <h1>Welcome to Imaraat</h1>
-      ) : (
-        <Fragment>
-          <HeroBanner />
-          <ListingBanner />
-          <Footer />
-        </Fragment>
-      )}
+    <div style={{ minHeight: "86vh" }} className="">
+      <HeroBanner />
+      <ListingBanner />
     </div>
   );
 };
